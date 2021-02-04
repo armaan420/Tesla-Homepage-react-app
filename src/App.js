542e89a7c4340d0c28940a2988bd4603ec2b7731
armaan singh <armaanmaani@gmail.com>
2021-02-04 11:35:26 -0500

    items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,99 @@
 import "./App.css";
 import Header from "./components/Header";
 
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Item from "./components/Item";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app_itemsContainer">
+        <Item
+          title="Lowest Cost Solar Panels"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="Order Now"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="$69,420"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="Money-back gurantee"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="Money-back gurantee"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="Money-back gurantee"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Accessories"
+          desc="Money-back gurantee"
+          descLink=""
+          backgroundImg={Accessories}
+          leftBtnTxt="Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar for New Roofs"
+          desc="Money-back gurantee"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="Custom Order"
+          leftBtnLink=""
+          rightBtnTxt="Learn More"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+      </div>
     </div>
   );
 }
